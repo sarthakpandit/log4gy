c8361ba17ec0137ab85ff010416996bc49884bb7
aureliengy <aureliengy@live.fr>
2011-09-02 14:38:59 +0000

    Brush about loglevels and global loglevels configuration

--- lib/Log4GY.php
+++ lib/Log4GY.php
@@ -10,26 +10,10 @@
  */
 abstract class Singleton{
 
-    //FIXME tenté de foutre le singleton en session mais ca marche pas un cul :')
-    public static function getSessionInstance(){
-
-        session_start();
-        
-        if(! isset($_SESSION['Log4GY_instances'])){
-            $_SESSION['Log4GY_instances'] = array(); 
-        }
-
-        $class = get_called_class();
-
-        if (! isset($_SESSION['Log4GY_instances'][$class])){
-
-            $_SESSION['Log4GY_instances'][$class] = new $class();
-
-        }
-        
-        return $_SESSION['Log4GY_instances'][$class];
-    }
-
+	/**
+	 * 
+	 * @return Singleton the singleton
+	 */
     public static function getInstance(){
 
         static $instances = array();
@@ -51,16 +35,6 @@ abstract class Singleton{
     protected function __clone() {
     }
 
-}
-
-/**
-* Classe contenant toutes les constantes statiques nécesssaires au fonctionnement de Log4GY.
-*
-* @version v0r0 (21 août 2011).
-* @author Effy - Aurélien GY <http://www.aureliengy.com>.
-*/
-class LogConst{
-    //TODO rajouter toutes les constantes
 }
 
 /**
@@ -96,7 +70,7 @@ class Log {
         
     }
 
-    public static function conf($name){
+    public static function conf($locale, $threshold, $logLevels, $tabAppenders = array(LogC::APP_CONSOLE), $singleFileOutput = true, $richFilePrepend = '<html><body>', $richFileAppend = '</body></html>'){
         
         $logger = Log4GY::getInstance();
 
@@ -165,8 +139,11 @@ class Log4GY extends Singleton{
         //TODO fermeture fichier.
     }
     
-    public function loadConfig($namefile){
-        $this->plop = $namefile;
+    public function loadConfig($locale, $threshold, $tabAppenders = array(LogC::APP_CONSOLE), $singleFileOutput = true){
+        // définition de la locale du serveur
+		setlocale(LC_ALL, $locale);
+    	
+    	$this->plop = $namefile;
     }
     
     public function log($mess){
@@ -303,3 +280,98 @@ class Log4GY extends Singleton{
     
 }
 
+class LogLevel {
+	
+	private $severity;
+	private $richFormat;
+	private $potFormat;
+	
+	public function __construct($severity, $richFormat = null, $potFormat = null){
+		
+		
+		$this->severity = $severity;
+		
+		if($potFormat == null){
+			$this->potFormat = '%1s %2s - %3s -> %4s'; //format par défaut
+		}
+		
+		if($richFormat == null){
+			$this->richFormat = '<p style="color:black;">%1s %2s - %3s -> %4s</p>'; //format par défaut
+		}
+
+	}
+	
+	public function getRichFormat(){
+		return $this->richFormat;
+	}
+	
+	public function getPotFormat(){
+		return $this->potFormat;
+	}
+	
+	public function isWished($threshold){
+		if($this->severity <= $threshold){
+			return true;
+		}
+		
+		return false;
+	}
+}
+
+
+/**
+* Classe contenant toutes les constantes statiques nécesssaires au fonctionnement de Log4GY.
+*
+* @version v0r0 (21 août 2011).
+* @author Effy - Aurélien GY <http://www.aureliengy.com>.
+*/
+class LogC{
+   
+	/**
+	 * Const définisant les différents niveaux de sévéritée de log
+	 */
+	const INFO 	= 10;	// Le plus verbeux - entrée / sortie dans une classe par exemple
+	const DEBUG = 20;	// Résultat d'une opération, valeur d'une variable, nombre d'instance d'un objets, d'occurences dans la base... 
+	const WARN 	= 30;	// Erreur faiblement impactante, tolérée dans l'application
+	const ERROR = 40;	// Erreur fortement impactante (dans un try - catch par exemple
+	const FATAL = 50;	// Erreur implicant l'impossibilitée de réaliser un action (session non disponible, droits fichiers inssuffisants, ...)
+	const OFF 	= 0;	// Pour désactiver les logs temporairement sans avoir à envlever les messages (par exemple pour passer l'application en production)
+	
+	
+	//TODO rajouter les implémentations de tous les appenders ca :)
+	/**
+	 * Const définissant les différents appenders (sorties) de log
+	 */
+	const APP_CONSOLE = 0; // Sortie echo
+	//const APP_STDERR = 1; // Sortie std_err
+	const APP_POTFILE = 2; // Sortie dans un fichier texte classique
+	const APP_RICHFILE = 3; // Sortie dans un fichier htm, avec colorisation en fonction de la sortie.
+	//const APP_BDD = 4; //Sortie dans une base de données
+	//const APP_BROWSERCONSOLE = 5; // Sortie console navigateur
+	
+	/**
+	 * Const générales de l'applications (regroupées pour être éventuellement modifiés par les utilisateurs du log
+	 */
+	const GENERAL_LOG_PATH = 'log';
+	const POTFILE_PATH = 'potfile';
+	const RICHFILE_PATH = 'richfile';
+	
+}
+
+/**
+ * Configuration des différents niveaux de sévérités gérés par l'application.
+ */
+$LogLevelInfo = new LogLevel(LogC::INFO, '<p style="color:blue;">%1s %2s - %3s -> %4s</p>'); 
+$LogLevelDebug = new LogLevel(LogC::DEBUG, '<p style="color:green;">%1s %2s - %3s -> %4s</p>'); 
+$LogLevelWarn = new LogLevel(LogC::WARN, '<p style="color:orange;">%1s %2s - %3s -> %4s</p>');
+$LogLevelError = new LogLevel(LogC::ERROR, '<p style="color:red;">%1s %2s - %3s -> %4s</p>');
+$LogLevelFatal = new LogLevel(LogC::FATAL, '<p style="color:darkred;">%1s %2s - %3s -> %4s</p>');
+
+$logLevels = array($LogLevelInfo, $LogLevelDebug, $LogLevelWarn, $LogLevelError, $LogLevelFatal);
+
+/**
+ * Configuration de l'ensemble des loggs
+ */
+
+//TODO vois si ya pas moyen de chopper direct la locale du serveur, c'est ptet suffisant ...
+Log::conf('fr_FR', LogC::OFF, $logLevels);
